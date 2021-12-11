722cf6ef5b7cba649e500a96373470d938c5b833
giovanni.carpentier <giovanni.carpentier@student.howest.be>
2021-12-11 15:34:12 +0100

    created needed recipe classes

---
+++ src/main/java/recipe/Ingredients.java
@@ -0,0 +1,17 @@
+package recipe;
+
+public class Ingredients {
+    String name;
+
+    public Ingredients(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/recipe/Recipe.java
@@ -0,0 +1,49 @@
+package recipe;
+
+import java.util.List;
+
+public class Recipe {
+    String name;
+    String description;
+    String instructions;
+    List<Ingredients> ingredients;
+
+    public Recipe(String name, String description, String instructions, List<Ingredients> ingredients) {
+        this.name = name;
+        this.description = description;
+        this.instructions = instructions;
+        this.ingredients = ingredients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public List<Ingredients> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredients> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
