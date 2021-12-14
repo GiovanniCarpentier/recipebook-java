f8468826e811c40433d3a9c3bf073b0a1f0399ac
giovanni.carpentier <giovanni.carpentier@student.howest.be>
2021-12-14 13:26:03 +0100

    updated MySQL connection

--- src/main/java/util/MySqlConnection.java
+++ src/main/java/util/MySqlConnection.java
@@ -1,17 +1,24 @@
 package util;
 
+import recipe.Recipe;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MySqlConnection {
     private static final String URL = "jdbc:mysql://localhost/recipebook";
     private static final String USERNAME = "java";
     private static final String PASSWORD = "java";
-    private static final Logger LOGGER = Logger.getLogger(ProductsDemo.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(Recipe.class.getName());
 
     public static void main(String[] args) {
         try (Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
             // use connection
         } catch (SQLException ex) {
-            LOGGER.log(Level.SEVERE, String.format("Error connecting to DB: %s", ex.getMessage());
-            throw new ShopException("Unable to connect to DB.");
+            LOGGER.log(Level.SEVERE, String.format("Error connecting to DB: %s", ex.getMessage()));
         }
     }
 }
